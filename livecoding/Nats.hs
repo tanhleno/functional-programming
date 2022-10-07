e54da9004e54ae736cf96b9e770f4f87fea4ce1d
Replit user <>
2022-10-07 18:30:31 +0000

    Update Show Nat

--- livecoding/Nats.hs
+++ livecoding/Nats.hs
@@ -3,11 +3,10 @@ module Nats where
 data Nat = Zero
          | Succ Nat
 
-plus :: Nat -> Nat -> Nat
-plus n Zero   = n
-plus n (Succ m) = Succ (plus n m)
-
 instance (Show Nat) where
-    show Zero   = "O"
-    show (Succ n) = "S" ++ show n
+    show Zero     = "O"
+    show (Succ n) = 'S' : show n
 
+plus :: Nat -> Nat -> Nat
+plus n Zero     = n
+plus n (Succ m) = Succ (plus n m)
